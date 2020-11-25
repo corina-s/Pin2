ea9c12c2730fb48cc63bd263bbf14c6c2e7b7984
corina-s <corina.schambacher@gmail.com>
2020-11-24 16:45:07 -0800

    added static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-
+  root 'static#index'
   namespace :v1, defaults: { format: 'json'} do
     get 'things', to: 'things#index'
   end
