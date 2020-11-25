a48db976409d27e69bb8bdf54ea1df3149cafb33
corina <corina.schambacher@gmail.com>
2020-11-25 15:41:46 -0800

    added store with middlewares added redux-logger and redux-thunk

---
+++ app/javascript/store/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducer/root_reducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+
+if(process.env.NODE_ENV !== 'production'){
+    middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {})=>(
+    createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(...middlewares)
+    )
+);
+
+export default configureStore;
