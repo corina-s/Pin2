7d26a93eafd60129964ade7c639890a701ca30e9
corina <corina.schambacher@gmail.com>
2020-11-25 20:11:58 -0800

    fixed bug in app rendering

---
+++ app/javascript/packs/pin2.jsx
@@ -0,0 +1,23 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../components/app'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // let store;
+    // // if (window.currentUser) {
+    // //     const preloadedState = { session: { id: window.currentUser.id }, entities: {users: {[window.currentUser.id]: window.currentUser}} };
+    // //     store = configureStore(preloadedState);
+    // //     delete window.currentUser;
+    // // } else {
+        // let store = configureStore();
+  
+    const root = document.getElementById("root");
+    
+    ReactDOM.render(<App/>, root);
+    // window.getState = store.getState;
+});
